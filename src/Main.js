2701eae72288f88efbc92c4905297f56703fd882
sahebjot <sahebjot.singh@juspay.in>
2017-05-02 20:07:39 +0530

    added own events for testing

--- src/Main.js
+++ src/Main.js
@@ -3,3 +3,18 @@ exports.move = function(sub) {
     sub({x: e.clientX, y: e.clientY})
   })
 }
+window.test = function(sub) {
+  return function(acsValue) {
+    var i = 0;
+    setInterval(function(){
+      i ++;
+      sub(acsValue + " " + i);
+    }, 3000)
+  }
+}
+
+exports.acsEvent = function(sub) {
+  window.start = window.test(sub)("testing");
+}
+
+
--- src/Main.purs
+++ src/Main.purs
@@ -23,6 +23,7 @@ import Data.Int (toNumber)
 
 
 foreign import move :: Event { x :: Int, y :: Int }
+foreign import acsEvent :: Event String
 
 type Circle = { x :: Number, y :: Number, size :: Number }
 
@@ -33,17 +34,18 @@ defaultSize :: Number
 defaultSize = 10.0
 
 
+
 scene :: { w :: Number, h :: Number } -> Behavior Drawing
-scene { w, h } = pure background <> (drawPoint <$> position <*> keys) where
+scene { w, h } = (drawPoint <$> position <*> keys) where
   background :: Drawing
   background = filled (fillColor white) (rectangle 0.0 0.0 w h)
 
   drawPoint :: {x :: Number, y :: Number} -> Set.Set Int -> Drawing
   drawPoint {x, y} inSet = case (Set.isEmpty inSet) of
-                                true  -> filled (fillColor black) (rectangle x y 10.0 10.0)
+                                true  -> filled (fillColor black) (rectangle x y defaultSize defaultSize)
                                 false -> case (Set.member 32 inSet) of
-                                              true  -> filled (fillColor black) (circle x y 10.0)
-                                              false -> filled (fillColor white) (circle x y 10.0)
+                                              true  -> filled (fillColor black) (circle x y defaultSize)
+                                              false -> filled (fillColor white) (circle x y defaultSize)
 {-
 scene2 :: { w :: Number, h :: Number } -> Behavior Drawing
 scene2 { w, h } = (drawPoint <$> position <*> keys) where
@@ -55,7 +57,12 @@ scene2 { w, h } = (drawPoint <$> position <*> keys) where
                                               false -> filled (fillColor white) (circle x y 10.0)
                                               -}
 
-main :: forall eff. Eff (canvas :: CANVAS, frp :: FRP | eff) Unit
+transformAcs str = str <> "foo"
+
+myBehav = transformAcs <$> (step "" acsEvent)
+
+
+main :: forall eff. Eff (canvas :: CANVAS, frp :: FRP, console :: CONSOLE | eff) Unit
 main = do
   mcanvas <- getCanvasElementById "canvas"
   let canvas = unsafePartial (fromJust mcanvas)
@@ -64,7 +71,9 @@ main = do
   h <- getCanvasHeight canvas
   _ <- setCanvasWidth w canvas
   _ <- setCanvasHeight h canvas
-  sample_ (scene {w,h}) (interval 10) `subscribe` render ctx
+  sample_ (scene {w,h}) (interval 20)`subscribe` render ctx
+  sample_ (scene {w,h}) move `subscribe` render ctx
+  sample_ myBehav (interval 1000) `subscribe` (\x -> log x)
 --  runContT (parTraverse_ (\s -> ContT ((sample_ s (interval 60)) `subscribe` render ctx)) [scene {w,h} , scene2 {w,h}]) logShow
 
 
